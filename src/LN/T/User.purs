ce521c408011fd41bf295dd9639a1b717105ff4a
Andrew Darqui <andrew.darqui@gmail.com>
2016-03-24 14:37:59 -0400

    testing

--- src/LN/T/User.purs
+++ src/LN/T/User.purs
@@ -39,19 +39,19 @@ derive instance genericUserRequest :: Generic UserRequest
 instance showUserRequest :: Show UserRequest where
   show = gShow
 
+mkUserRequest :: String -> UserRequest
+mkUserRequest nick = UserRequest { nick }
+
 defaultUserRequest :: UserRequest
 defaultUserRequest = mkUserRequest "empty"
-
-mkUserRequest :: String -> UserRequest
-mkUserRequest n = UserRequest { nick: n }
 
 _UserRequest :: LensP UserRequest { nick :: String }
 _UserRequest f (UserRequest o) = UserRequest <$> f o
 
-_nick :: forall b a r. Lens { nick :: a | r } { nick :: b | r } a b
-_nick f o = o { nick = _ } <$> f o.nick
+_req_nick :: forall b a r. Lens { nick :: a | r } { nick :: b | r } a b
+_req_nick f o = o { nick = _ } <$> f o.nick
 
-instance encodeUser :: EncodeJson UserRequest where
+instance encodeUserRequest :: EncodeJson UserRequest where
   encodeJson (UserRequest u) =
        "nick"  := u.nick
     ~> jsonEmptyObject
@@ -80,15 +80,53 @@ instance isForeignUserRequest :: IsForeign UserRequest where
 
 
 newtype UserResponse = UserResponse {
-  userResponseId          :: UserId,
-  userResponseNick        :: String,
-  userResponseDisplayNick :: String
+  id   :: UserId,
+  nick :: String
 }
+
+derive instance genericUserResponse :: Generic UserResponse
+
+instance showUserResponse :: Show UserResponse where
+  show = gShow
 
 defaultUserResponse :: UserResponse
-defaultUserResponse = UserResponse {
-  userResponseId: 0,
-  userResponseNick: "",
-  userResponseDisplayNick: ""
-}
+defaultUserResponse = mkUserResponse 0 "empty"
 
+mkUserResponse :: Int -> String -> UserResponse
+mkUserResponse id nick = UserResponse { id, nick }
+
+_UserResponse :: LensP UserResponse { id :: Int, nick :: String }
+_UserResponse f (UserResponse o) = UserResponse <$> f o
+
+_resp_id :: forall b a r. Lens { id :: a | r } { id :: b | r } a b
+_resp_id f o = o { id = _ } <$> f o.id
+
+_resp_nick :: forall b a r. Lens { nick :: a | r } { nick :: b | r } a b
+_resp_nick f o = o { nick = _ } <$> f o.nick
+
+instance encodeUserResponse :: EncodeJson UserResponse where
+  encodeJson (UserResponse u) =
+       "id"    := u.id
+    ~> "nick"  := u.nick
+    ~> jsonEmptyObject
+
+instance decodeUserResponse :: DecodeJson UserResponse where
+  decodeJson = gDecodeJson
+
+instance respondableUserResponse :: Respondable UserResponse where
+  responseType =
+    Tuple Nothing JSONResponse
+  fromResponse json =
+    mkUserResponse
+           <$> readProp "id" json
+           <*> readProp "nick" json
+
+instance requestableUserResponse :: Requestable UserResponse where
+  toRequest s =
+    let str = printJson (encodeJson s) :: String
+     in toRequest str
+
+instance isForeignUserResponse :: IsForeign UserResponse where
+  read f = mkUserResponse
+    <$> readProp "id" f
+    <*> readProp "nick" f
