bd648d0a240de52df45000fea66cad6b098c7ba3
Andrew Darqui <andrew.darqui@gmail.com>
2016-03-26 02:20:35 -0400

    odkfdsokfods

--- src/LN/Api/ThreadPosts.purs
+++ src/LN/Api/ThreadPosts.purs
@@ -21,3 +21,18 @@ getThreadPosts = do
          (Right js) -> do
            liftAff $ log "success"
            return $ Just js
+
+
+
+getThreadPost :: forall eff. Int -> Aff (ajax :: AJAX, console :: CONSOLE | eff) (Maybe ThreadPostResponse)
+getThreadPost thread_post_id = do
+    liftAff $ log "getThreadPost"
+    { response: response } <- AJ.get ("api/thread_posts/" <> show thread_post_id)
+    let r = fromResponse response
+    case r of
+         (Left err) -> do
+           liftAff $ log $ ("error: " ++ show err)
+           return Nothing
+         (Right js) -> do
+           liftAff $ log "success"
+           return $ Just js
