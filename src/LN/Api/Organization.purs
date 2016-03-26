26d692f5e8fbce974552f3a572bda38b26fe76bb
Andrew Darqui <andrew.darqui@gmail.com>
2016-03-26 18:33:03 -0400

    getOrg

--- src/LN/Api/Organization.purs
+++ src/LN/Api/Organization.purs
@@ -21,3 +21,18 @@ getOrganizations = do
          (Right js) -> do
            liftAff $ log "success"
            return $ Just js
+
+
+
+getOrganization :: forall eff. Int -> Aff (ajax :: AJAX, console :: CONSOLE | eff) (Maybe OrganizationResponses)
+getOrganization org_id = do
+    liftAff $ log "getOrganization"
+    { response: response } <- AJ.get ("api/orgs/" ++ show org_id)
+    let r = fromResponse response
+    case r of
+         (Left err) -> do
+           liftAff $ log $ ("error: " ++ show err)
+           return Nothing
+         (Right js) -> do
+           liftAff $ log "success"
+           return $ Just js
