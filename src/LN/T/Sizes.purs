34da1d00f92b227f63193bb96057da1f180c9bd3
Andrew Darqui <andrew.darqui@gmail.com>
2016-04-01 02:56:17 -0400

    generic size

---
+++ src/LN/T/Sizes.purs
@@ -0,0 +1,27 @@
+module LN.T.Sizes where
+
+
+
+import LN.T.Prelude
+import Data.Generic
+
+
+
+data Size
+  = XSmall
+  | Small
+  | Medium
+  | Large
+  | XLarge
+
+
+derive instance genericSize :: Generic Size
+
+instance eqSize :: Eq Size where
+  eq = gEq
+
+instance ordSize :: Ord Size where
+  compare = gCompare
+
+instance showSize :: Show Size where
+  show = gShow
