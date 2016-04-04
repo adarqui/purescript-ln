d37a680067ff865d6009e3b19c3c624dcb91de02
Andrew Darqui <andrew.darqui@gmail.com>
2016-04-04 05:04:40 -0400

    retrieve packs

---
+++ src/LN/Api/Packs/Board.purs
@@ -0,0 +1,28 @@
+module LN.Api.Packs.Board where
+
+
+
+import LN.T.Prelude
+import LN.Api.Prelude
+import LN.Api.Internal
+import LN.T.Packs.Board
+
+
+
+getBoardsPacks :: ApiEff (Maybe BoardPackResponses)
+getBoardsPacks = getAt [] [] [boardsPacksTag]
+
+
+
+getBoardsPacksByForumId :: String -> ApiEff (Maybe BoardPackResponses)
+getBoardsPacksByForumId forum = getAt [] [ByForumId forum] [boardsPacksTag]
+
+
+
+getBoardPack :: String -> ApiEff (Maybe BoardPackResponse)
+getBoardPack board_id = getAt [] [] [boardsPacksTag, board_id]
+
+
+
+getBoardPackByForumId :: String -> String -> ApiEff (Maybe BoardPackResponse)
+getBoardPackByForumId forum board = getAt [] [ByForumId forum] [boardsPacksTag, board]
