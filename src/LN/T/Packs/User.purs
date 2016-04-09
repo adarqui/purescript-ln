609bae337780d9ec92a1827b0ef50512fc5f1f99
Andrew Darqui <andrew.darqui@gmail.com>
2016-04-09 19:29:17 -0400

    user stats and user pack

---
+++ src/LN/T/Packs/User.purs
@@ -0,0 +1,138 @@
+module LN.T.Packs.User where
+
+
+
+import LN.T.Prelude.Argonaut
+import LN.T.User
+import LN.T.User.Response.Sanitized
+import LN.T.User.Stat.Response.Sanitized
+import LN.T.Profile
+import LN.T.Profile.Response
+import LN.T.DateMaybe
+
+
+
+userPackTag :: String
+userPackTag = "user_pack"
+
+
+
+usersPacksTag :: String
+usersPacksTag = "users_packs"
+
+
+
+newtype UserPackResponse = UserPackResponse {
+  user :: UserSanitizedResponse,
+  userStat :: UserSanitizedStatResponse,
+  userProfile :: ProfileResponse
+}
+
+
+
+_UserPackResponse :: LensP UserPackResponse {
+    user :: UserSanitizedResponse,
+    userStat :: UserSanitizedStatResponse,
+    userProfile :: ProfileResponse
+  }
+_UserPackResponse f (UserPackResponse o) = UserPackResponse <$> f o
+
+
+
+defaultUserPackResponse :: UserPackResponse
+defaultUserPackResponse =
+  mkUserPackResponse defaultUserSanitizedResponse defaultUserSanitizedStatResponse defaultProfileResponse
+
+
+
+mkUserPackResponse :: UserSanitizedResponse -> UserSanitizedStatResponse -> ProfileResponse -> UserPackResponse
+mkUserPackResponse user userStat userProfile =
+  UserPackResponse {user, userStat, userProfile}
+
+
+
+
+instance encodeUserPackResponse :: EncodeJson UserPackResponse where
+  encodeJson (UserPackResponse o) =
+       "user"         := o.user
+    ~> "user_stat"    := o.userStat
+    ~> "user_profile" := o.userProfile
+    ~> jsonEmptyObject
+
+
+
+instance decodeUserPackResponse :: DecodeJson UserPackResponse where
+  decodeJson json = do
+    obj <- decodeJson json
+    user <- obj .? "user"
+    userStat <- obj .? "user_stat"
+    userProfile <- obj .? "user_profile"
+    pure $ UserPackResponse {user, userStat, userProfile}
+
+
+
+
+instance respondableUserPackResponse :: Respondable UserPackResponse where
+  responseType =
+    Tuple Nothing JSONResponse
+  fromResponse f =
+    mkUserPackResponse
+      <$> readProp "user" f
+      <*> readProp "user_stat" f
+      <*> readProp "user_profile" f
+
+
+
+instance requestableUserPackResponse :: Requestable UserPackResponse where
+  toRequest s =
+    let str = printJson (encodeJson s) :: String
+     in toRequest str
+
+
+
+instance isForeignUserPackResponse :: IsForeign UserPackResponse where
+  read f = mkUserPackResponse
+    <$> readProp "user" f
+    <*> readProp "user_stat" f
+    <*> readProp "user_profile" f
+
+
+
+
+newtype UserPackResponses = UserPackResponses {
+  usersPacks :: Array UserPackResponse
+}
+
+defaultUserPackResponses :: UserPackResponses
+defaultUserPackResponses = mkUserPackResponses
+
+mkUserPackResponses :: UserPackResponses
+mkUserPackResponses = UserPackResponses { usersPacks: [] }
+
+instance encodeUserPackResponses :: EncodeJson UserPackResponses where
+  encodeJson (UserPackResponses o) =
+       usersPacksTag  := o.usersPacks
+    ~> jsonEmptyObject
+
+instance decodeUserPackResponses :: DecodeJson UserPackResponses where
+  decodeJson json = do
+    obj <- decodeJson json
+    usersPacks <- obj .? usersPacksTag
+    pure $ UserPackResponses { usersPacks: usersPacks }
+
+instance respondableUserPackResponses :: Respondable UserPackResponses where
+  responseType =
+    Tuple Nothing JSONResponse
+  fromResponse json = do
+    usersPacks <- readProp usersPacksTag json
+    pure $ UserPackResponses { usersPacks: usersPacks }
+
+instance requestableUserPackResponses :: Requestable UserPackResponses where
+  toRequest s =
+    let str = printJson (encodeJson s) :: String
+     in toRequest str
+
+instance isForeignUserPackResponses :: IsForeign UserPackResponses where
+  read f = do
+    usersPacks <- readProp usersPacksTag f
+    pure $ UserPackResponses { usersPacks: usersPacks }
---
+++ src/LN/T/User/Stat/Response/Sanitized.purs
@@ -0,0 +1,166 @@
+module LN.T.User.Stat.Response.Sanitized where
+
+
+
+import LN.T.Prelude.Argonaut
+import LN.T.User
+
+
+
+newtype UserSanitizedStatResponse = UserSanitizedStatResponse {
+  userId   :: UserId,
+  threads :: Int,
+  threadPosts :: Int,
+  respect :: Int,
+  resources :: Int,
+  leurons :: Int,
+  workouts :: Int
+}
+
+_UserSanitizedStatResponse :: LensP UserSanitizedStatResponse {
+    userId   :: UserId,
+    threads :: Int,
+    threadPosts :: Int,
+    respect :: Int,
+    resources :: Int,
+    leurons :: Int,
+    workouts :: Int
+  }
+_UserSanitizedStatResponse f (UserSanitizedStatResponse o) = UserSanitizedStatResponse <$> f o
+
+
+
+defaultUserSanitizedStatResponse :: UserSanitizedStatResponse
+defaultUserSanitizedStatResponse = mkUserSanitizedStatResponse 0 0 0 0 0 0 0
+
+
+
+mkUserSanitizedStatResponse :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> UserSanitizedStatResponse
+mkUserSanitizedStatResponse userId threads threadPosts respect resources leurons workouts =
+  UserSanitizedStatResponse { userId, threads, threadPosts, respect, resources, leurons, workouts }
+
+
+
+instance encodeUserSanitizedStatResponse :: EncodeJson UserSanitizedStatResponse where
+  encodeJson (UserSanitizedStatResponse o) =
+       "user_id"      := o.userId
+    ~> "threads"      := o.threads
+    ~> "thread_posts" := o.threadPosts
+    ~> "respect"      := o.respect
+    ~> "resources"    := o.resources
+    ~> "leurons"      := o.leurons
+    ~> "workouts"     := o.workouts
+    ~> jsonEmptyObject
+
+
+
+instance decodeUserSanitizedStatResponse :: DecodeJson UserSanitizedStatResponse where
+  decodeJson json = do
+    obj <- decodeJson json
+    userId <- obj .? "user_id"
+    threads <- obj .? "threads"
+    threadPosts <- obj .? "thread_posts"
+    respect <- obj .? "respect"
+    resources <- obj .? "resources"
+    leurons <- obj .? "leurons"
+    workouts <- obj .? "workouts"
+    pure $ UserSanitizedStatResponse {userId, threads, threadPosts, respect, resources, leurons, workouts}
+
+
+
+instance respondableUserSanitizedStatResponse :: Respondable UserSanitizedStatResponse where
+  responseType =
+    Tuple Nothing JSONResponse
+  fromResponse json =
+    mkUserSanitizedStatResponse
+      <$> readProp "user_id" json
+      <*> readProp "threads" json
+      <*> readProp "thread_posts" json
+      <*> readProp "respect" json
+      <*> readProp "resources" json
+      <*> readProp "leurons" json
+      <*> readProp "workouts" json
+
+
+
+instance requestableUserSanitizedStatResponse :: Requestable UserSanitizedStatResponse where
+  toRequest s =
+    let str = printJson (encodeJson s) :: String
+     in toRequest str
+
+
+
+instance isForeignUserSanitizedStatResponse :: IsForeign UserSanitizedStatResponse where
+  read f = mkUserSanitizedStatResponse
+    <$> readProp "user_id" f
+    <*> readProp "threads" f
+    <*> readProp "thread_posts" f
+    <*> readProp "respect" f
+    <*> readProp "resources" f
+    <*> readProp "leurons" f
+    <*> readProp "workouts" f
+
+
+
+
+
+
+
+
+
+newtype UserSanitizedStatResponses = UserSanitizedStatResponses {
+  usersStats :: Array UserSanitizedStatResponse
+}
+
+_UserSanitizedStatResponses :: LensP UserSanitizedStatResponses {
+    usersStats :: Array UserSanitizedStatResponse
+  }
+_UserSanitizedStatResponses f (UserSanitizedStatResponses o) = UserSanitizedStatResponses <$> f o
+
+
+
+defaultUserSanitizedStatResponses :: UserSanitizedStatResponses
+defaultUserSanitizedStatResponses = mkUserSanitizedStatResponses
+
+
+
+mkUserSanitizedStatResponses :: UserSanitizedStatResponses
+mkUserSanitizedStatResponses = UserSanitizedStatResponses { usersStats: [] }
+
+
+
+instance encodeUserSanitizedStatResponses :: EncodeJson UserSanitizedStatResponses where
+  encodeJson (UserSanitizedStatResponses u) =
+       "users_stats"    := u.usersStats
+    ~> jsonEmptyObject
+
+
+
+instance decodeUserSanitizedStatResponses :: DecodeJson UserSanitizedStatResponses where
+  decodeJson json = do
+    obj <- decodeJson json
+    usersStats <- obj .? "users_stats"
+    pure $ UserSanitizedStatResponses { usersStats: usersStats }
+
+
+
+instance respondableUserSanitizedStatResponses :: Respondable UserSanitizedStatResponses where
+  responseType =
+    Tuple Nothing JSONResponse
+  fromResponse json = do
+    usersStats <- readProp "users_stats" json
+    pure $ UserSanitizedStatResponses { usersStats: usersStats }
+
+
+
+instance requestableUserSanitizedStatResponses :: Requestable UserSanitizedStatResponses where
+  toRequest s =
+    let str = printJson (encodeJson s) :: String
+     in toRequest str
+
+
+
+instance isForeignUserSanitizedStatResponses :: IsForeign UserSanitizedStatResponses where
+  read f = do
+    usersStats <- readProp "users_stats" f
+    pure $ UserSanitizedStatResponses { usersStats: usersStats }
