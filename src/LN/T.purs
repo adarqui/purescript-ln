dbf0f54b39f7d19d3ae13c802fb108e28baa60e6
Andrew Darqui <andrew.darqui@gmail.com>
2016-04-28 12:02:39 -0400

    needed stuff that wasnt auto generated

--- src/LN/T.purs
+++ src/LN/T.purs
@@ -3,15 +3,18 @@ module LN.T (
 ) where
 
 
+
 -- These are only found in purescript-ln
 --
+import           LN.T.Param                          as A
+import           LN.T.Profile.Helper                 as A
 {-
 import           LN.T.Date                           as A
 import           LN.T.DateMaybe                      as A
 import           LN.T.Lens.Field                     as A
-import           LN.T.Profile.Helper                 as A
 import           LN.T.Size                           as A
 -}
+
 
 
 import LN.T.Internal.Types as A
