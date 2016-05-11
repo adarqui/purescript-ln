422ac344dc5794105e65af7682a20eef04341b9f
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-11 15:20:29 -0400

    some helpers

---
+++ src/LN/T/Pack/ThreadPost/Response.purs
@@ -0,0 +1,15 @@
+module LN.T.Pack.ThreadPost.Response (
+  defaultThreadPostPackResponse
+) where
+
+
+
+import Data.Maybe           (Maybe(..))
+import LN.T.Internal.Types  (ThreadPostResponse, UserSanitizedResponse
+                            , ThreadPostPackResponse, mkThreadPostPackResponse)
+import LN.T.ThreadPost.Stat (defaultThreadPostStatResponse)
+
+
+
+defaultThreadPostPackResponse :: ThreadPostResponse -> UserSanitizedResponse -> ThreadPostPackResponse
+defaultThreadPostPackResponse thread_post user = mkThreadPostPackResponse thread_post user defaultThreadPostStatResponse Nothing
---
+++ src/LN/T/ThreadPost/Stat.purs
@@ -0,0 +1,12 @@
+module LN.T.ThreadPost.Stat (
+  defaultThreadPostStatResponse
+) where
+
+
+
+import LN.T.Internal.Types (ThreadPostStatResponse, mkThreadPostStatResponse)
+
+
+
+defaultThreadPostStatResponse :: ThreadPostStatResponse
+defaultThreadPostStatResponse = mkThreadPostStatResponse 0 0 0 0 0
