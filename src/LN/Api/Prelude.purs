3d54c2961cbb5fbf03fe99787cdb324ad9fcef7b
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-20 13:55:23 -0400

    not needed

--- src/LN/Api/Prelude.purs
+++ src/LN/Api/Prelude.purs
@@ -4,14 +4,13 @@ module LN.Api.Prelude (
 
 
 
-import LN.T.Prelude as A
-import Data.Maybe as A
-import Control.Monad.Aff as A
-import Control.Monad.Aff.Class as A
-import Data.Foreign.Class as A
-import Network.HTTP.Affjax as A
-import Network.HTTP.Affjax.Request as A
-import Network.HTTP.Affjax.Response as A
-import Network.HTTP.RequestHeader as A
-import Control.Monad.Eff.Console (CONSOLE) as A
-import Control.Monad.Aff.Console (log) as A
+import  Data.Maybe                    as         A
+import  Control.Monad.Aff             as         A
+import  Control.Monad.Aff.Class       as         A
+import  Data.Foreign.Class            as         A
+import  Network.HTTP.Affjax           as         A
+import  Network.HTTP.Affjax.Request   as         A
+import  Network.HTTP.Affjax.Response  as         A
+import  Network.HTTP.RequestHeader    as         A
+import  Control.Monad.Eff.Console     (CONSOLE)  as  A
+import  Control.Monad.Aff.Console     (log)      as  A
